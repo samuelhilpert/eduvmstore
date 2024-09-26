5355dfb2831ff8450ac16a7b88fbaeb3f339ff74
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-09-26 13:41:31 +0200

    edit ruff.toml to fix issues with line-length

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 # Hallo
 
-def HalloWelt():
+def hallo_welt():
     print("Hallo Welt")
 
     print("1234567890dajklfhfljkadhjkfhadjkfhjkladshfadhskljfhadjkfhlajkdshfjkladshfkjadshfkjhadsjkfhadlkjfhadljshfkjasdhflkahdskljfhasdkljfhadsjhfhd")
+
 def unbenutzte_funktion():
     pass
 
 import os, sys
 
-class testClass:
+class TestClass:
     def __init__(self):
         self.wert = 0
 
-    def setWert(self, wert):
+    def set_wert(self, wert):
         self.wert = wert
